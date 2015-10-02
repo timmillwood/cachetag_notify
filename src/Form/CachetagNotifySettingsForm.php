5e15a7f875af2332f0ed422d7342bbc41050bcb1
Tim Millwood <tim@millwoodonline.co.uk>
2015-10-02 10:09:34 +0100

    Adding validation to settings form.

--- src/Form/CachetagNotifySettingsForm.php
+++ src/Form/CachetagNotifySettingsForm.php
@@ -3,8 +3,10 @@
 namespace Drupal\cachetag_notify\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
-use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\UrlHelper;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\RequestException;
 
 /**
  * Defines a form to configure module settings.
@@ -46,6 +48,15 @@ class CachetagNotifySettingsForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if (!$this->isEndpointValid($form_state->getValue('endpoint'))) {
+      $form_state->setErrorByName('endpoint', $this->t('Invalid endpoint URL.'));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = $this->config('cachetag_notify.settings');
 
@@ -54,5 +65,26 @@ class CachetagNotifySettingsForm extends ConfigFormBase {
       ->save();
 
     parent::submitForm($form, $form_state);
+  }
+
+  protected function isEndpointValid($endpoint) {
+    if (UrlHelper::isValid($endpoint, TRUE)) {
+      try {
+        $response = \Drupal::httpClient()->post($endpoint, []);
+        if ($response->getStatusCode() === 200) {
+          return TRUE;
+        }
+        return FALSE;
+      }
+      catch (ConnectException $e) {
+        return FALSE;
+      }
+      catch (RequestException $e) {
+        return FALSE;
+      }
+      catch (Exception $e) {
+        return FALSE;
+      }
+    }
   }
 }
